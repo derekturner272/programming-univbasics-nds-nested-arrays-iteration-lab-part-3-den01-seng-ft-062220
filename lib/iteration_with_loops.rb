fc25a4627544927714a29f41d2c78e7c4660b4a3
Derek Turner <turner.derek.b@gmail.com>
2020-06-05 05:41:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     
     while second_index < src[first_index].count do
       if src[first_index][second_index].is_a?(String)
-        new_form.push(src[first_index][second_index])
+        new_form.push(src[first_index][second_index] )
       end
       second_index += 1
     end
