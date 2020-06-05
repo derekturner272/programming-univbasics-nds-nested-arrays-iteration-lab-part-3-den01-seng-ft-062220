7f4badb32c33a43b719df9d673f074cf8b3aee3e
Derek Turner <turner.derek.b@gmail.com>
2020-06-05 05:29:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,18 @@
 def join_nested_strings(src)
 
+  low_temps = []
+  day_index = 0
+  while day_index < src.count do
+    temp_index = 0
+    dayly_low = 1000
+    while temp_index < src[day_index].count do
+      if dayly_low > src[day_index][temp_index]
+        dayly_low = src[day_index][temp_index]
+      end
+      temp_index += 1
+    end
+    low_temps.push(dayly_low)
+    day_index += 1
+  end
+  low_temps
 end
